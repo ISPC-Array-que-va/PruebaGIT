33224ec93527f6a45610925405bf93e2a1865bce
Iván Bürcher <ivanb_1997@hotmail.com>
2026-02-15 17:20:21 -0300

    Ahora el archivo se llama datos, Agregué función para total gastos

--- datos.py
+++ datos.py
@@ -1,1 +1,3 @@
 nombres =  ["Iván", "Pilar", "Carolina", "Marcela"]
+
+gastos = [2390.3, 25.5, 5929, 205]
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import print as mo
+import datos as mo
 
-
+#ESCRIBIR GASTOS
 for nombre in mo.nombres:
     print(nombre, end=" ")
+print("\n \n")
 
-
+#SUMAR GASTOS
+total_gastos = 0
+for i in mo.gastos:
+    total_gastos = total_gastos + i
+print("Total de Gastos:", total_gastos, "\n")
