99b24f1892851f69f1ba07cb0f62bd2adf3ddac8
Iván Bürcher <ivanb_1997@hotmail.com>
2026-02-17 20:40:29 -0300

    Arreglos menores

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import datos as mo
 
 #ESCRIBIR GASTOS
 for nombre in mo.nombres:
-    print(nombre, end=" - ")
+    print(nombre, end=" ")
 print("\n \n")
 
 #SUMAR GASTOS
 total_gastos = 0
 for i in mo.gastos:
     total_gastos = total_gastos + i
-print("Total de Gastos:", total_gastos, "\n")
+print("Total de Gastos:", total_gastos)
 
 #SUMAR ingresos
 total_ingresos = 0
 for i in mo.ingresos:
     total_ingresos = total_ingresos + i
-print("Total de Ingresos:", total_ingresos, "\n")
+print("Total de Ingresos:", total_ingresos)
 
 #Saldo
 Saldo = total_ingresos - total_gastos
 
 print("Saldo = ", Saldo, "\n")
-
 
 print("desarrollador: Iván Bürcher")
